d2ced273b77be49e57703713bb58b6062580b763
kenttyyy <kentajrins123@gmail.com>
2025-05-26 19:15:49 +0800

    class and mainloop

--- oop_revision.py
+++ oop_revision.py
@@ -26,3 +26,19 @@ class Question:
             for key, value in self.choices.items():
                 file.write(f"{key}. {value}\n")
             file.write(f"Correct Answer: {self.correct_answer}\n\n")
+
+class QuizCreator:
+    def __init__(self):
+        self.colors = AnsiColor()
+
+    # [4] move menu and user input logic into QuizCreator methods
+    def display_menu(self):
+        print(f"{self.colors.BOLD}{self.colors.CYAN}What do you want to do?")
+        print(f"{self.colors.YELLOW}Type 1 - Add A Question")
+        print(f"{self.colors.YELLOW}Type 2 - Exit")
+
+    def get_user_choice(self):
+        try:
+            return int(input(f"{self.colors.MAGENTA}Enter your choice 1 or 2: "))
+        except ValueError:
+            return -1
