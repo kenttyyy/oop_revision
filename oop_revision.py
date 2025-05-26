68225b2f7c690a82f0f568094fa15e3c723caac0
kenttyyy <kentajrins123@gmail.com>
2025-05-26 19:11:07 +0800

    save to file

--- oop_revision.py
+++ oop_revision.py
@@ -8,3 +8,14 @@ class AnsiColor:
     MAGENTA = "\033[95m"
     BOLD = "\033[1m"
     RESET = "\033[0m"
+
+class Question:
+    def __init__(self, text, choice_a, choice_b, choice_c, choice_d, correct_answer):
+        self.text = text
+        self.choices = {
+            "A": choice_a,
+            "B": choice_b,
+            "C": choice_c,
+            "D": choice_d
+        }
+        self.correct_answer = correct_answer
