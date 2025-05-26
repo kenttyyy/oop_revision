7eb528fd5324b26bbd979ab872d0fc70423accd7
kenttyyy <kentajrins123@gmail.com>
2025-05-26 19:07:58 +0800

    ansi color constants

---
+++ oop_revision.py
@@ -0,0 +1,10 @@
+# Program 9
+
+class AnsiColor:
+    RED = "\033[91m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    CYAN = "\033[96m"
+    MAGENTA = "\033[95m"
+    BOLD = "\033[1m"
+    RESET = "\033[0m"
